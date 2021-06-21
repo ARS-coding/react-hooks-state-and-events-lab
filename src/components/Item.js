683363a91681dfbb6920dc70b84a0ecade1dbc80
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-21 21:55:15 +0300

    Implement the adding to cart function using state.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,15 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
 
+  let [isAddedToCart, setIsAddedToCart] = useState(false);
+
   return (
-    <li className="">
+    <li className={isAddedToCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={(e) => {
-        e.target.parentElement.classList.toggle("in-cart")
-      }}>Add to Cart</button>
+      <button className="add" onClick={() => setIsAddedToCart(!isAddedToCart)}>Add to Cart</button>
     </li>
   );
 }
