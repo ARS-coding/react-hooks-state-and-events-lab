a90dd8d0d5e5985fc8f92c7f36bffb396123f21b
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-21 21:51:40 +0300

    Add the adding to cart functionality.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Item({ name, category }) {
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={(e) => {
+        e.target.parentElement.classList.toggle("in-cart")
+      }}>Add to Cart</button>
     </li>
   );
 }
